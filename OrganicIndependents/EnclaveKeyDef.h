45e1bacbb996797bea393f5ead0f2a5166aceaec
bigans01 <bigans01@gmail.com>
2020-09-02 08:34:59 -0400

    Add project files.

---
+++ OrganicIndependents/EnclaveKeyDef.h
@@ -0,0 +1,73 @@
+/*------------------------------------------------------------------------------------------
+
+
+Initial creation:        7/7/2017
+--EnclaveKeyDef.h		(Last update 11/11/2019) (Moved to OrganicPolyOperationsLib)
+
+Description: Header file for EnclaveKeyDef.cpp.
+
+Summary: EnclaveKeyDef.h supplies the definition that will be used for the key values in any type of collection. The Key is made up of a x/y/z coordinate, and is meant to be put
+into an unordered map. A hash for this is then constructed, in accordance with the rules for an unordered map. Examples of classes that use this are:
+
+-EnclaveCollectionMap (member variable: EnclaveCollectionMap)
+-ManifestCollectionMap (member variable: ManiCollectionMap)
+-RenderCollectionMap (member variable: RenderMatrix)
+
+Dependents: none
+
+
+------------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#ifndef ENCLAVEKEY_H
+#define ENCLAVEKEY_H
+
+#include <iostream>
+
+class EnclaveKeyDef {
+
+public:
+	// forms the structure of the Key itself.
+	struct EnclaveKey
+	{
+		int x = 0;
+		int y = 0;
+		int z = 0;
+
+		// operator overloading (required)
+		bool operator==(const EnclaveKey &other) const
+		{
+			return (x == other.x
+				&&
+				y == other.y
+				&&
+				z == other.z);
+		}
+	};
+
+	struct Enclave2DKey			// for use with EnclaveUnveilMeta only (subject to change later)
+	{
+		int a = 0;
+		int b = 0;
+	};
+
+
+	struct KeyHasher
+	{
+		// hash generation function, required to be specified when put into an unordered map. 
+		std::size_t operator()(const EnclaveKey& k) const
+		{
+			using std::size_t;
+			using std::hash;
+			using std::string;
+			return (
+				(hash<int>()(k.x)
+					^ (hash<int>()(k.y) << 1)
+					) >> 1)
+				^ (hash<int>()(k.z) << 1);
+		}
+	};
+};
+
+#endif
---
+++ OrganicIndependents/EuclideanDirection3D.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef EUCLIDEANDIRECTION3D_H
+#define EUCLIDEANDIRECTION3D_H
+
+enum class EuclideanDirection3D
+{
+	POS_X,	// east
+	POS_Z,	// north
+	NEG_X,	// west
+	NEG_Z,	// south
+	POS_Y,  // above
+	NEG_Y	// below
+};
+
+#endif
