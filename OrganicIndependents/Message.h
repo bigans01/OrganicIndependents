a09e37861042e9cb144d8c9c4e1369eb35e1526b
bigans01 <bigans01@gmail.com>
2020-09-25 10:31:00 -0400

    --updated Message header file, and MessageType enum.

--- OrganicIndependents/Message.h
+++ OrganicIndependents/Message.h
@@ -4,16 +4,58 @@
 #define MESSAGE
 
 #include "MessageType.h"
+#include "EnclaveKeyDef.h"
 #include <vector>
 
 class Message
 {
 	public:
 		Message() {};
+		Message& operator=(const Message& message_b)
+		{
+			messageType = message_b.messageType;
+			intVector = message_b.intVector;
+			//intVectorIter = intVector.begin();
+			messageID = message_b.messageID;
+			return *this;
+		}
 		Message(int in_messageID, MessageType in_messageType) : messageID(in_messageID), messageType(in_messageType) {};
 		MessageType messageType = MessageType::NOVAL;
 		std::vector<int> intVector;
+		std::vector<int>::iterator intVectorIter;
 		int messageID = 0;				// set by the constructor.
+
+		void insertEnclaveKey(EnclaveKeyDef::EnclaveKey in_key)
+		{
+			intVector.push_back(in_key.x);
+			intVector.push_back(in_key.y);
+			intVector.push_back(in_key.z);
+		}
+
+		void insertInt(int in_int)
+		{
+			intVector.push_back(in_int);
+		}
+
+		EnclaveKeyDef::EnclaveKey readEnclaveKey()
+		{
+			EnclaveKeyDef::EnclaveKey returnKey;
+			returnKey.x = *intVectorIter++;
+			returnKey.y = *intVectorIter++;
+			returnKey.z = *intVectorIter++;
+			return returnKey;
+		}
+
+		int readInt()
+		{
+			return *intVectorIter++;
+		}
+
+		void open()
+		{
+			intVectorIter = intVector.begin();
+		}
+
 };
 
 #endif
