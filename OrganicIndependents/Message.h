c7e0407b249ef593f6ffe1ff5ae466a948b82ea6
bigans01 <bigans01@gmail.com>
2020-10-02 12:09:32 -0400

    --added functionality to insert and read floats to the Message class.

--- OrganicIndependents/Message.h
+++ OrganicIndependents/Message.h
@@ -18,6 +18,7 @@ class Message
 			messageLocality = message_b.messageLocality;
 			messageType = message_b.messageType;
 			intVector = message_b.intVector;
+			floatVector = message_b.floatVector;
 			stringVector = message_b.stringVector;
 			messageID = message_b.messageID;
 			return *this;
@@ -28,9 +29,11 @@ class Message
 		MessageType messageType = MessageType::NOVAL;
 
 		std::vector<int> intVector;
+		std::vector<float> floatVector;
 		std::vector<std::string> stringVector;
 		
 		std::vector<int>::iterator intVectorIter;
+		std::vector<float>::iterator floatVectorIter;
 		std::vector<std::string>::iterator stringVectorIter;
 
 		int messageID = 0;				// set by the constructor.
@@ -45,6 +48,11 @@ class Message
 		void insertInt(int in_int)
 		{
 			intVector.push_back(in_int);
+		}
+
+		void insertFloat(float in_float)
+		{
+			floatVector.push_back(in_float);
 		}
 
 		void insertString(std::string in_string)
@@ -66,6 +74,11 @@ class Message
 			return *intVectorIter++;
 		}
 
+		float readFloat()
+		{
+			return *floatVectorIter++;
+		}
+
 		std::string readString()
 		{
 			return *stringVectorIter++;
@@ -74,6 +87,7 @@ class Message
 		void open()
 		{
 			intVectorIter = intVector.begin();
+			floatVectorIter = floatVector.begin();
 			stringVectorIter = stringVector.begin();
 		}
 
