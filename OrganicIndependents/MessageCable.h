eb3dae41bad53bed029bd5a2f34f8e5f357a6c4f
bigans01 <bigans01@gmail.com>
2020-10-02 20:56:15 -0400

    --the MessageCable class has had its functionality upgraded to be thread-safe, via lock guards.
    --added a new message type, REQUEST_FROM_SERVER_SEND_BLUEPRINTS_FOR_OGLMBUFFERMANAGER

--- OrganicIndependents/MessageCable.h
+++ OrganicIndependents/MessageCable.h
@@ -4,13 +4,71 @@
 #define MESSAGECABLE_H
 
 #include <queue>
+#include <mutex>
 #include "Message.h"
 
 class MessageCable
 {
-	public:	
-		std::queue<Message> outgoingMessages;	// put requests (for instance, for a client's request)
+	private:
 		std::queue<Message> incomingMessages;	// put responses (for instance, what a server responds to the client with)
+		std::mutex incomingQueueGuard;
+
+		std::queue<Message> outgoingMessages;	// put requests (for instance, for a client's request)
+		std::mutex outgoingQueueGuard;
+	public:
+		// incoming message functions
+		void insertIncomingMessage(Message in_message)
+		{
+			std::lock_guard<std::mutex> lock(incomingQueueGuard);
+			incomingMessages.push(in_message);
+		}
+
+		void popIncomingQueue()
+		{
+			std::lock_guard<std::mutex> lock(incomingQueueGuard);
+			incomingMessages.pop();
+		}
+
+		bool isIncomingQueueEmpty()
+		{
+			std::lock_guard<std::mutex> lock(incomingQueueGuard);
+			return incomingMessages.empty();
+		}
+
+		Message* getIncomingMessageRefFromFront()
+		{
+			std::lock_guard<std::mutex> lock(incomingQueueGuard);
+			return &incomingMessages.front();
+		}
+
+
+
+		// outgoing message functions
+		void insertOutgoingMessage(Message in_message)
+		{
+			std::lock_guard<std::mutex> lock(outgoingQueueGuard);
+			outgoingMessages.push(in_message);
+		}
+
+		void popOutgoingQueue()
+		{
+			std::lock_guard<std::mutex> lock(outgoingQueueGuard);
+			outgoingMessages.pop();
+		}
+
+		bool isOutgoingQueueEmpty()
+		{
+			std::lock_guard<std::mutex> lock(outgoingQueueGuard);
+			return outgoingMessages.empty();
+		}
+
+		Message* getOutgoingMessageRefFromFront()
+		{
+			std::lock_guard<std::mutex> lock(outgoingQueueGuard);
+			return &outgoingMessages.front();
+		}
+
+
 };
 
 #endif
