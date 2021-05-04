1ecb29bca9a6e21d0a516ebe052352dba5dc3776
bigans01 <bigans01@gmail.com>
2021-05-04 13:56:55 -0400

    --code for determining the ECBPPOrientation of an Enclave point has been moved from the OrganicPolyOperationsLib (higher level library) to this library; see the new function IndependentUtils::GetEnclavePointOrientation.

---
+++ OrganicIndependents/EnclaveBorderLineList.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "BlockBorderCornerPoint.h"
+#include "BlockBorder.h"
+#include <iostream>
+
+#ifndef ENCLAVEBORDERLINELIST_H
+#define ENCLAVEBORDERLINELIST_H
+
+class EnclaveBorderLineList
+{
+public:
+	EnclaveBorderLineList();
+
+	// Type 1 borders (corners)
+	BlockBorderCornerPoint  corner_UpperNW;
+	BlockBorderCornerPoint  corner_UpperSW;
+	BlockBorderCornerPoint  corner_UpperSE;
+	BlockBorderCornerPoint  corner_UpperNE;
+
+	BlockBorderCornerPoint  corner_LowerNW;
+	BlockBorderCornerPoint  corner_LowerSW;
+	BlockBorderCornerPoint  corner_LowerSE;
+	BlockBorderCornerPoint  corner_LowerNE;
+
+	// Type 2 borders (lines)
+	BlockBorder Zaxis_lowerWest;
+	BlockBorder Zaxis_lowerEast;
+	BlockBorder Zaxis_upperWest;
+	BlockBorder Zaxis_upperEast;
+
+	BlockBorder Xaxis_lowerNorth;
+	BlockBorder Xaxis_lowerSouth;
+	BlockBorder Xaxis_upperNorth;
+	BlockBorder Xaxis_upperSouth;
+
+	BlockBorder Yaxis_northWest;
+	BlockBorder Yaxis_northEast;
+	BlockBorder Yaxis_southWest;
+	BlockBorder Yaxis_southEast;
+
+	// Type 1 borders (faces)
+	BlockBorder WestFace;
+	BlockBorder NorthFace;
+	BlockBorder EastFace;
+	BlockBorder SouthFace;
+	BlockBorder TopFace;
+	BlockBorder BottomFace;
+};
+
+#endif
--- OrganicIndependents/IndependentUtils.h
+++ OrganicIndependents/IndependentUtils.h
@@ -6,12 +6,14 @@
 #include "ECBPPOrientationResults.h"
 #include "ECBPolyPoint.h"
 #include "BlockBorderLineList.h"
+#include "EnclaveBorderLineList.h"
 
 class IndependentUtils
 {
 	public:
 		static ECBPPOrientationResults GetPointOrientation(ECBPolyPoint in_pointToCheck, BlockBorderLineList in_blockBorders);	// default version of GetPointOrientation
 		static ECBPPOrientationResults GetPointOrientation(ECBPolyPoint in_pointToCheck, BlockBorderLineList* in_blockBorders);	// version of GetPointOrientation which takes a pointer instead of a copy of in_blockBorders
+		static ECBPPOrientationResults GetEnclavePointOrientation(ECBPolyPoint in_pointToCheck, EnclaveBorderLineList* in_enclaveBorderLineList);
 		static ECBPolyPoint roundPolyPointToHundredths(ECBPolyPoint in_pointToCheck);
 		static float roundToHundredth(float in_float);
 };
