bb5d51befb88a92f39811c88d527ee465245bdcb
bigans01 <bigans01@gmail.com>
2021-09-03 16:29:44 -0400

    --added non-default constructor to PolyLogger class.

--- OrganicIndependents/PolyLogger.h
+++ OrganicIndependents/PolyLogger.h
@@ -8,26 +8,27 @@
 
 class PolyLogger
 {
-private:
-	PolyDebugLevel logLevel = PolyDebugLevel::NONE;
-public:
-	void setDebugLevel(PolyDebugLevel in_debugLevel);
-	template<typename First, typename ...Rest> void log(First && first, Rest && ...rest)
-	{
-		//std::cout << "Hey we should be debugging here..." << std::endl;
-		//std::cout << std::endl;
-		if (logLevel == PolyDebugLevel::DEBUG)
+	public:
+		PolyLogger() {};
+		PolyLogger(PolyDebugLevel in_logLevel) :
+			logLevel(in_logLevel)
+		{};
+
+		void setDebugLevel(PolyDebugLevel in_debugLevel);
+		template<typename First, typename ...Rest> void log(First && first, Rest && ...rest)
 		{
-			//(std::cout << ... << args);
-			std::cout << std::forward<First>(first);
-			log(std::forward<Rest>(rest)...);
+			if (logLevel == PolyDebugLevel::DEBUG)
+			{
+				std::cout << std::forward<First>(first);
+				log(std::forward<Rest>(rest)...);
+			}
 		}
-		//std::cout << std::endl;
-	}
-	void log() {};
-	bool isLoggingSet();
-	void waitForDebugInput();
-	PolyDebugLevel getLogLevel();
+		void log() {};
+		bool isLoggingSet();
+		void waitForDebugInput();
+		PolyDebugLevel getLogLevel();
+	private:
+		PolyDebugLevel logLevel = PolyDebugLevel::NONE;
 };
 
 #endif
